dc96c5980dd5fc1eefff6efe24f8c88c0bd23d23
Bobcho101 <bgbobi878@gmail.com>
2025-02-14 17:32:12 +0200

    made a function that gets the user's preferred devices

--- src/services/profile-service.js
+++ src/services/profile-service.js
@@ -3,3 +3,7 @@ import Device from "../models/DeviceModel.js";
 export async function getUserCreatedDevices(userId) {
     return await Device.find({ creator: userId});
 }
+
+export async function getUserPreferredDevices(userId) {
+    return await Device.find({ prefferedList: userId});
+}
